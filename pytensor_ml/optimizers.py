cafcfa0c22e3ef7a3c2cc57072a4ad9c3cf4aca9
jessegrabowski <jessegrabowski@gmail.com>
2024-04-20 23:54:29 +0200

    Add ADAGrad optimizer

--- pytensor_ml/optimizers.py
+++ pytensor_ml/optimizers.py
@@ -10,10 +10,16 @@ import numpy as np
 
 class Optimizer(ABC):
 
-    def __init__(self, model: Model, loss_fn: Callable, ndim_out: int=1):
+    def __init__(self,
+                 model: Model,
+                 loss_fn: Callable,
+                 ndim_out: int=1,
+                 optimizer_weights: list[TensorVariable] | None =None):
         self.model = model
         self.loss_fn = loss_fn
         self.ndim_out = ndim_out
+        self.optimizer_weights = optimizer_weights if optimizer_weights is not None else []
+        self._optimizer_weights_values = self._initialize_weights()
 
         self.update_fn = self.build_update_fn()
 
@@ -22,6 +28,23 @@ class Optimizer(ABC):
     def update_parameters(self, params: Sequence[TensorVariable], loss: TensorVariable):
         ...
 
+    @property
+    def optimizer_weights_values(self) -> list[np.ndarray]:
+        return self._optimizer_weights_values
+
+    @optimizer_weights_values.setter
+    def optimizer_weights_values(self, values: list[np.ndarray]):
+        for i, new_value in enumerate(values):
+            self._optimizer_weights_values[i][:] = new_value
+
+    def _initialize_weights(self) -> list[np.ndarray]:
+        if self.optimizer_weights:
+            return [np.zeros(param.type.shape) for param in self.optimizer_weights]
+        return []
+
+    def _split_weights(self, all_weights: list[TensorLike]) -> tuple[list[TensorLike], list[TensorLike]]:
+        n_params = len(self.model.weights)
+        return all_weights[:n_params], all_weights[n_params:]
 
     def build_update_fn(self) -> Function:
         """
@@ -42,11 +65,13 @@ class Optimizer(ABC):
         label_slice = (slice(None),) * self.ndim_out + (0, ) * (y_hat.ndim - self.ndim_out)
         y = y_hat[np.s_[label_slice]].type()
 
-        params = self.model.weights
+        weights = self.model.weights
+        optimizer_weights = self.optimizer_weights
 
         loss = self.loss_fn(y, y_hat)
-        new_parameters = self.update_parameters(params, loss)
-        fn = function([x, y, *params], [*new_parameters, loss])
+
+        all_weights = self.update_parameters(weights + optimizer_weights, loss)
+        fn = function([x, y, *weights, *optimizer_weights], [*all_weights, loss])
         return fn
 
     def step(self, x_values, y_values) -> np.ndarray:
@@ -57,9 +82,13 @@ class Optimizer(ABC):
         -------
         loss
         """
-        *new_weights, loss_values = self.update_fn(x_values, y_values, *self.model.weight_values)
-        self.model.weight_values = new_weights
+        *new_weights, loss_values = self.update_fn(x_values, y_values,
+                                                   *self.model.weight_values,
+                                                   *self.optimizer_weights_values)
         self.update_fn.trust_input = True
+
+        self.model.weight_values, self.optimizer_weights_values  = self._split_weights(new_weights)
+
         return loss_values
 
 
@@ -75,3 +104,27 @@ class SGD(Optimizer):
             new_params.append(param - self.learning_rate * d_loss_d_param)
 
         return new_params
+
+
+class ADAGrad(Optimizer):
+    def __init__(self, model, loss_fn, *, ndim_out:int=1, learning_rate: TensorLike = 0.01, epsilon: TensorLike = 1e-8):
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon
+        g2_weights = [param.type() for param in model.weights]
+
+        super().__init__(model, loss_fn, ndim_out=ndim_out, optimizer_weights=g2_weights)
+
+    def update_parameters(self, weights: Sequence[TensorVariable], loss: TensorVariable) -> list[TensorVariable]:
+        weights, optimizer_weights = self._split_weights(weights)
+        grads = grad(loss, weights)
+
+        new_weights = []
+        new_optimizer_weights = []
+
+        for param, d_loss_d_param, g2 in zip(weights, grads, optimizer_weights):
+            new_g2 = g2 + d_loss_d_param ** 2
+            weight_update = d_loss_d_param / np.sqrt(new_g2 + self.epsilon)
+            new_weights.append(param - self.learning_rate * weight_update)
+            new_optimizer_weights.append(new_g2)
+
+        return new_weights + new_optimizer_weights
