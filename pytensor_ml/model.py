4c0b244513d0e33047d9905869f89c5531fe913b
jessegrabowski <jessegrabowski@gmail.com>
2024-04-20 21:43:49 +0200

    Use getter/setter pattern for model.weight_values

--- pytensor_ml/model.py
+++ pytensor_ml/model.py
@@ -1,5 +1,7 @@
+from functools import partial
 from typing import Sequence, cast, Literal, Generator
 
+from pytensor.compile.function import function
 from pytensor.tensor import TensorVariable, TensorType
 from pytensor.compile.sharedvalue import SharedVariable
 from pytensor.printing import debugprint
@@ -9,6 +11,9 @@ from pytensor import config
 
 import numpy as np
 from pytensor_ml.layers import Layer
+
+
+InitializationSchemes = Literal['zeros', 'xavier_uniform', 'xavier_normal']
 
 
 def required_graph_inputs(tensor: TensorVariable) -> Generator[TensorVariable, None, None]:
@@ -24,31 +29,49 @@ class Model:
         self.X = X
         self.y = y
 
-        self.weight_values: list[np.ndarray[float]] | None = None
+        self._weight_values: list[np.ndarray[float]] | None = None
+        self._predict_fn = None
 
-
-    def initalize_weights(self) -> dict[TensorVariable, TensorType]:
-        raise NotImplementedError
+    def initalize_weights(self,
+                          scheme: InitializationSchemes = 'xavier_normal',
+                          random_seed: int | str | np.random.Generator | None = None):
+        self._weight_values = initialize_weights(self, scheme, random_seed)
 
     @property
     def weights(self) -> list[TensorVariable]:
         return [var for var in required_graph_inputs(self.y) if var is not self.X]
 
+    @property
+    def weight_values(self) -> list[np.ndarray[float]]:
+        if self._weight_values is None:
+            raise ValueError("Weights have not been initialized")
+        return self._weight_values
+
+    @weight_values.setter
+    def weight_values(self, values: list[np.ndarray[float]]):
+        for i, new_value in enumerate(values):
+            self._weight_values[i][:] = new_value
+
+    def predict(self, X_values: np.ndarray) -> np.ndarray:
+        if self._predict_fn is None:
+            f = function([*self.weights, self.X], self.y)
+            self._predict_fn = partial(f, *self.weight_values)
+
+        return self._predict_fn(X_values)
+
     def __str__(self):
         return debugprint(self.y, file="str")
 
-
-Schemes = Literal['zeros', 'xavier_uniform', 'xavier_normal']
 
 def _zero_init(shape: tuple[int], *args) -> np.ndarray:
     return np.zeros(shape, dtype=config.floatX)
 
 def _xavier_uniform_init(shape: tuple[int], rng: np.random.Generator) -> np.ndarray:
-    scale = np.sqrt(6.0 / np.sum(shape))
+    scale = np.sqrt(6.0 / np.sum([x for x in shape if x is not None]))
     return rng.uniform(-scale, scale, size=shape).astype(config.floatX)
 
 def _xavier_normal_init(shape: tuple[int], rng: np.random.Generator) -> np.ndarray:
-    scale = np.sqrt(2.0 / np.sum(shape))
+    scale = np.sqrt(2.0 / np.sum([x for x in shape if x is not None]))
     return rng.normal(0, scale, size=shape).astype(config.floatX)
 
 
@@ -58,7 +81,7 @@ initialization_factory = {
     'xavier_normal': _xavier_normal_init
 }
 
-def initialize_weights(model, scheme: Schemes, random_seed: int | str | np.random.Generator | None):
+def initialize_weights(model, scheme: InitializationSchemes, random_seed: int | str | np.random.Generator | None):
     if isinstance(random_seed, str):
         random_seed = sum(map(ord, random_seed))
     if isinstance(random_seed, int) or random_seed is None:
